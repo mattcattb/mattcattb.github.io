102247af45433d276135dabc7d9d3090ae976dab
mattcattb <mboughton17@gmail.com>
2024-11-26 13:36:56 -0500

    added about for homepage, added readme

--- user-app/src/components/About.jsx
+++ user-app/src/components/About.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-
-import '../styles/About.css';
-
+import React from 'react';
+import { Box, Typography, Avatar } from '@mui/material';
 import profile from '../assets/img/mattypfp.jpeg';
 
 export default function About() {
   return (
-    <section className="about-section">
-        <img className='profile-picture' src={profile} alt="Matthew Boughton"/>
-        <h1>About me!</h1>
-        <p>
-            Hey! My names Matthew. Im a Computer Science student at the University of Florida who loves to get his hands dirty.
-            Im constantly fascinated by the world around us, and want to dive into any system I can find. You can follow my 
-            github to see what im working on, or read through my blog to see a deeper dive into what im doing!
-        </p>
-    </section>
-  )
+    <Box 
+      sx={{ 
+        textAlign: 'center', 
+        padding: 4, 
+        maxWidth: 600, 
+        margin: 'auto', 
+        boxShadow: 3, 
+        borderRadius: 2, 
+        bgcolor: 'background.paper' 
+      }}
+    >
+      <Avatar 
+        src={profile} 
+        alt="Matthew Boughton" 
+        sx={{ width: 120, height: 120, margin: 'auto', marginBottom: 2 }}
+      />
+      <Typography variant="h1" sx={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: 2 }}>
+        About Me!
+      </Typography>
+      <Typography variant="body1">
+        Hey! My name's Matthew. I'm a Computer Science student at the University of Florida who loves to get his hands dirty. 
+        I'm constantly fascinated by the world around us and want to dive into any system I can find. You can follow my GitHub 
+        to see what I'm working on, or read through my blog to see a deeper dive into what I'm doing!
+      </Typography>
+    </Box>
+  );
 }
