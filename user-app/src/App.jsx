60c7766dda1d1eca974e46e0e4396c7582b6926c
mattcattb <mboughton17@gmail.com>
2024-11-26 13:35:54 -0500

    added color theme to app

--- user-app/src/App.jsx
+++ user-app/src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { links, routes, posts } from './routesConfig';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Customize primary color
+    },
+    secondary: {
+      main: '#f50057', // Customize secondary color
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif', // Change global font
+    h1: { fontSize: '2.5rem', fontWeight: 700 },
+    body1: { lineHeight: 1.6 },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Navbar links={links} />
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route key={path} path={path} element={React.cloneElement(element, { posts })} />
-        ))}
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <Navbar links={links} />
+        <Routes>
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={React.cloneElement(element, { posts })} />
+          ))}
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
